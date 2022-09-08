7f3af86869e4f06f31bbbbdd496ec5b659a83347
Jorge Garcia <joregat@gmail.com>
2022-09-08 12:31:20 -0300

    Ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'salaJuegos';
+
+  edadUno:number = 0;
+  edadDos:number = 0;
+  promedio:number = 0;
+  suma:number = 0;
+
+  public calcular(){
+    this.calcularSuma();
+    this.calcularPromedio();
+  }
+
+  private calcularSuma()
+  {
+    this.suma = this.edadUno + this.edadDos;
+    return this.suma;
+  }
+
+  private calcularPromedio()
+  {
+    this.promedio = this.calcularSuma()/2;
+  }
 }
